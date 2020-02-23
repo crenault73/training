a2e330390de7a42aab9f684016badbd33c28e984
Christophe Renault <platyphyllos@gmail.com>
2020-02-23 23:51:37 +0100

    ajout variables

--- python/tutograven/src/01_bases/cre_bases.py
+++ python/tutograven/src/01_bases/cre_bases.py
@@ -1,1 +1,7 @@
+def main():
+    print("Hello World!")
+    print("Salut à tous.")
 
+
+if __name__ == '__main__':
+    main()
---
+++ python/tutograven/src/02_variables/cre_var.py
@@ -0,0 +1,11 @@
+def main():
+    #Création d'une variable 'username' ayant pour valeur 'crenault'
+    username = "crenault"
+    #Création d'une variable 'age' ayant pour valeur 46
+    age = 46
+    #...
+    wallet=125.8
+
+
+if __name__ == '__main__':
+    main()
