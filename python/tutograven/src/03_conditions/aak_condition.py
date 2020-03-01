49a0ecc6ebdfeeb6f2c1e94a9be27322e0ae4443
akloul amira <amira.akloul@ens.uvsq.fr>
2020-03-02 00:07:45 +0100

    Ajout de trois nouveaux fichiers dans le repertoire 03_conditions

---
+++ python/tutograven/src/03_conditions/aak_condition.py
@@ -0,0 +1,10 @@
+
+#code
+wallet = 5000
+computer_price = 1200
+
+# le prix de l'ordinateur est inferieur à 1000 euros <>
+if computer_price < 1000:
+    print("le prix de l'ordinateur est bien inferieur à 1000 euros")
+else:
+    print("Non, le prix de l'ordinateur est superieur à 1000 euros")
