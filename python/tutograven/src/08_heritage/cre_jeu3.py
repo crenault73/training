032d6ede0751b545527198ccc950b7732f94a45d
Christophe Renault <platyphyllos@gmail.com>
2020-03-09 22:39:21 +0100

    cre: modif indent

--- python/tutograven/src/08_heritage/cre_jeu3.py
+++ python/tutograven/src/08_heritage/cre_jeu3.py
@@ -1,5 +1,6 @@
 from model.cre_player import Player, Warrior
-#from model.cre_player import Warrior
+
+# from model.cre_player import Warrior
 
 player = Player("Graven", 20, 2)
 warrior = Warrior("DarkWarrior", 30, 4, 3)
@@ -8,4 +9,4 @@ warrior.damage(4)
 warrior.damage(5)
 
 if issubclass(Warrior, Player):
-      print("Le guerrier est bien une spécialisation de Player.")
+    print("Le guerrier est bien une spécialisation de Player.")
