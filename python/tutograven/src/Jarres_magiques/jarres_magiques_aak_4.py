be0967f6e465a7284c231004062745d4cb04a7c2
akloul amira <amira.akloul@ens.uvsq.fr>
2020-04-20 21:39:06 +0200

    Ajout de fichiers dans le répetoire jarres_magiques

--- python/tutograven/src/Jarres_magiques/jarres_magiques_aak_4.py
+++ python/tutograven/src/Jarres_magiques/jarres_magiques_aak_4.py
@@ -2,28 +2,33 @@ import random
 
 print("Bienvenue dans le jeu !")
 
+# choix du paramétrage
+
 # compteur clés magiques
 keys = 0
 
 while keys != 3:
-    print("Vous disposez de 5 jarres, chosissez 1, 2, 3, 4 ou 5")
-
-    # le choix aléatoire de la jarre qui fait perdre notre joueur
-    snake_jar = random.randint(1, 5)  # nombre au hasard entre 1 et 5
+    level = int(input('Quel niveau de difficulté pour le jeu ? 1, 2 ou 3 ? : '))
+    snake = ['s'] * level
+    difference = 5 - level
+    cle = ['k'] * difference
+    jars = snake + cle
+    random.shuffle(jars)
+    print(jars)
 
     # demander a notre joueur de mettre une valeur
     choix = int(input("choisissez un numéro de jarre : "))
 
     # vérification
 
-    if choix != snake_jar:
-        print("Gagné ! Vous obtenez une clé magique ! la jarre infectée etait la numéro ", snake_jar)
+    if jars[choix-1] == 'k':
+        print("Gagné ! Vous obtenez une clé magique ! la jarre infectée etait la numéro ", jars)
         keys += 1
         print("Vous avez actuellement : ", keys, "/3 clés")
     else:
         print("Perdu ! un serpent apparait !")
 
-        #si le joueur n'a pas gagné de clé
+        #si le joueur n'a pas de clé
         if keys > 0:
             keys -= 1
             print("Vous avez actuellement : ", keys, "/3 clés")
